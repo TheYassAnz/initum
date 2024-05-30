92127f590d789d04e2b9606b1fa7e43a34a1f1fe
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-30 13:43:28 +0200

    feat: config tailwindcss and prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-// Local imports
-import Home from './pages/Home'
-import Header from './components/Header'
-import Error from './pages/Error'
-
-
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
-
-  </React.StrictMode>
-);
-
-
+    <React.StrictMode>
+        <h1>Hello world!</h1>
+    </React.StrictMode>
+)
