72a8c3ceec05da7913bcec74faddb8a9ac4b9416
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-31 23:52:23 +0200

    fix: redirect directly to /home when the app start

--- src/index.jsx
+++ src/index.jsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
         element: <Root />,
         children: [
             {
+                index: true,
+                element: <Navigate to="/home" />,
+            },
+            {
                 path: 'home',
                 element: <Home />,
             },
