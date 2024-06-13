7cae4538fd84bf859f1946bc67cc2576cb6e3865
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-06-13 14:18:36 +0200

    feat: verify recaptcha token

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,9 @@ const p = dotenv.config().parsed;
 // nodemailer
 const nodemailer = require('nodemailer');
 
+// axios
+const axios = require('axios');
+
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -17,8 +20,6 @@ var transporter = nodemailer.createTransport({
         pass: p.TRANSPORT_PASSWORD,
     },
 });
-// // import database connection
-// const { db } = require('./config/db');
 
 // Middleware which intercept JSON data
 app.use(express.json());
@@ -54,24 +55,49 @@ app.get('/', (req, res, next) => {
 
 // simple middleware
 app.post('/api/mail/send', (req, res) => {
-    const { firstname, lastname, email, phone, project, message } = req.body;
-    transporter.sendMail(
-        {
-            to: 'yassanz.contact@gmail.com, alievabubakar@mail.com',
-            cc: 'yassanz.webmaster@gmail.com',
-            subject: `Nouveau message de ${firstname} ${lastname} - ${project}`,
-            text: `Nom: ${firstname} ${lastname}\nEmail: ${email}\nTéléphone: ${phone}\nProjet: ${project}\nMessage: ${message}`,
-        },
-        function (error, info) {
-            if (error) {
+    const {
+        firstname,
+        lastname,
+        email,
+        phone,
+        project,
+        message,
+        captchaToken,
+    } = req.body;
+    try {
+        axios
+            .post(
+                `https://www.google.com/recaptcha/api/siteverify?secret=${p.RECAPTCHA_SERVER_KEY}&response=${captchaToken}`
+            )
+            .then((response) => {
+                if (!response.data.success) {
+                    res.status(500).json({ message: 'Invalid Captcha' });
+                } else {
+                    transporter.sendMail(
+                        {
+                            to: 'yassanz.contact@gmail.com, alievabubakar@mail.com',
+                            cc: 'yassanz.webmaster@gmail.com',
+                            subject: `Nouveau message de ${firstname} ${lastname} - ${project}`,
+                            text: `Nom: ${firstname} ${lastname}\nEmail: ${email}\nTéléphone: ${phone}\nProjet: ${project}\nMessage: ${message}`,
+                        },
+                        function (error, info) {
+                            if (error) {
+                                res.status(500).json({ message: error });
+                            } else {
+                                res.status(200).json({
+                                    message: `Message envoyé avec succès!`,
+                                });
+                            }
+                        }
+                    );
+                }
+            })
+            .catch((error) => {
                 res.status(500).json({ message: error });
-            } else {
-                res.status(200).json({
-                    message: `Message envoyé avec succès!`,
-                });
-            }
-        }
-    );
+            });
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
 });
 
 // export the express app
